7b67a8cc9016ff49e0076d25d028d80417d8664d
ZalohaD <dinis7794@gmail.com>
2025-08-15 23:16:41 +0300

    Add more checks

--- includes/class-plugin.php
+++ includes/class-plugin.php
@@ -5,35 +5,40 @@ if (!defined('ABSPATH')) exit;
 
 require_once plugin_dir_path(__FILE__) . 'class-checkout-controller.php';
 
-
 class Plugin {
 
     public function init() {
-        add_filter('template_include', [$this, 'load_custom_checkout_template']);
+        add_filter('template_include', [$this, 'load_custom_checkout_template'], 99);
+
         add_action('wp_enqueue_scripts', [$this, 'enqueue_styles']);
+        add_action('wp_enqueue_scripts', [$this, 'enqueue_scripts']);
 
         new CheckoutController();
     }
 
     public function enqueue_styles() {
-        if(is_checkout()):
+        if (function_exists('is_checkout') && is_checkout()):
             wp_enqueue_style(
                 'bootstrap-css',
                 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css'
             );
             wp_enqueue_style(
                 'checkout-css',
-                plugin_dir_url(__DIR__) . 'assets/css/style.css', false, '1.2'
+                plugin_dir_url(__FILE__) . '../assets/css/style.css',
+                [],
+                '1.2'
             );
         endif;
     }
 
-    public function enqueue_scripts()
-    {
-        if(is_checkout()):
+    public function enqueue_scripts() {
+        if (function_exists('is_checkout') && is_checkout()):
             wp_enqueue_script(
                 'checkout-js',
-                plugin_dir_url(__DIR__) . 'assets/js/custom-checkout.js', array(), '1.1',
+                plugin_dir_url(__FILE__) . '../assets/js/custom-checkout.js',
+                ['jquery'],
+                '1.1',
+                true
             );
         endif;
     }
@@ -44,4 +49,5 @@ class Plugin {
         endif;
         return $template;
     }
+
 }
